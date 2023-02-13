6e01de94e9746243e4b5166c4518dea6483b5f39
Gabriel Azevedo <gs.techcode@gmail.com>
2023-02-12 22:12:23 -0300

    Alteração e remoção de alguns arquivos desnecessários

--- Setup.py
+++ Setup.py
@@ -5,7 +5,14 @@ import os
 
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
-build_exe_options = {"packages": ["os","unidecode","pyautogui","datetime","zipfile","PyQt5","json","tkinter","shutil","reportlab","PIL","subprocess","psutil"]}
+build_exe_options = {"packages": ["os","unidecode","pyautogui","datetime","zipfile","PyQt5","json","tkinter","shutil","reportlab","PIL","subprocess","psutil"], "include_files" : [
+    ("./Cache","./Cache"),
+    ("./Images","./Images"),
+    ("./Tools","./Tools"),
+    ("./Fonts","./Fonts"),
+    ("./Models","./Models"),
+    (".version",".version")
+    ], "excludes": ["*"]}
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
@@ -13,9 +20,9 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(
-    name="PyRelator",
+    name="Sunshine",
     version="1.0",
-    description="PyRelator",
+    description="Sunshine",
     options={"build_exe": build_exe_options},
-    executables=[Executable("PyRelator.py", base=base, icon= os.getcwd() + r"\Images\Icon.ico")],
+    executables=[Executable("Sunshine.py", base=base, icon= os.getcwd() + r"\Images\Icon.ico")],
 )
